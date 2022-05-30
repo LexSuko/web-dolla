36a320cc21ddff1f6a4d133afba9635ed74945e9
AP <ap@APs-MacBook-Pro.local>
2022-05-30 16:51:32 +0700

    build and update code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SigninPage from './pages/signin';
 
 function App() {
   return (
-    <Router>
+    <Router path="/web-dolla">
       <Routes>
         <Route path="/" element={<Home/>} exact/>
         <Route path="/signin" element={<SigninPage/>} exact/>
