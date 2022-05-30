3234138f9903b2c7e379844663cc65eca58734a6
AP <ap@APs-MacBook-Pro.local>
2022-05-30 17:23:09 +0700

    build and update code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages';
 import SigninPage from './pages/signin';
 // import Animate from './pages/animate';
 
 function App() {
   return (
-    <HashRouter basename={'/web-dolla'}>
+    <Router>
       <Routes>
         <Route path="/web-dolla" element={<Home/>} exact/>
         <Route path="/signin" element={<SigninPage/>} exact/>
         {/* <Route path="/" element={<Animate/>} exact/> */}
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
